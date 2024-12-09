eb686e6b305f8201928fbf3de387de305a5dc3cb
bumgoo <bumgoo4417@gmail.com>
2024-12-09 15:04:50 +0900

    Initial commit

---
+++ src/main/java/com/example/patbingsu/PatbingsuApplication.java
@@ -0,0 +1,13 @@
+package com.example.patbingsu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatbingsuApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PatbingsuApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/patbingsu/PatbingsuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.patbingsu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatbingsuApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
